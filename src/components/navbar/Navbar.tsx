69afc0aa44b28aec1b6f749859b98bf5d2b9bfbb
CaciaRodrigues <caciarodriguues@gmail.com>
2025-01-17 14:25:42 -0300

    Modificando Navbar.tsx

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,19 +1,33 @@
-import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthContext";
 
 function Navbar() {
+
+  const navigate = useNavigate();
+
+  const {handleLogOut} = useContext(AuthContext)
+
+  function logout() {
+    handleLogOut()
+    alert('O Usuário foi desconctado com sucesso!')
+    navigate('/')
+  }
+
   return (
     <>
-      <div
-        className="w-full flex justify-center py-4
-            			   bg-indigo-900 text-white"
-      >
+      <div className="w-full bg-indigo-900 text-white flex justify-center py-4" >
         <div className="container flex justify-between text-lg">
           <Link to="/home" className="text-2xl font-bold">
             Blog Pessoal
           </Link>
 
           <div className="flex gap-4">
-            Postagens Temas Cadastrar tema Perfil Sair
+            Postagens 
+            Temas 
+            Cadastrar Tema 
+            Perfil 
+            <Link to='' onClick={logout} className="hover:underline">Sair</Link>
           </div>
         </div>
       </div>
