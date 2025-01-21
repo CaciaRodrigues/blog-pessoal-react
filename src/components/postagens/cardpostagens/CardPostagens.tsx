5d1b1232bf497c7dcfa6dc180eb98396f7c7d671
CaciaRodrigues <caciarodriguues@gmail.com>
2025-01-21 11:41:14 -0300

    Adicionando as informações persistidas no banco de dados ao Card Postagem

--- src/components/postagens/cardpostagens/CardPostagens.tsx
+++ src/components/postagens/cardpostagens/CardPostagens.tsx
@@ -1,18 +1,26 @@
 import { Link } from "react-router-dom";
+import Postagem from "../../../models/Postagem";
 
-function CardPostagens() {
+interface CardPostagensProps {
+    postagem: Postagem
+}
+
+function CardPostagens({postagem}: CardPostagensProps) {
     return (
         <div className="border-rose-quartz border flex flex-col rounded overflow-hidden justify-between">
             <div>
                 <div className="flex w-full bg-lit-eggplant-purple py-2 px-4 items-center gap-4">
-                    <img src="https://i.imgur.com/pK6vSCy.png" className="h-12 rounded-full" alt="" />
-                    <h3 className="text-lg  text-lavender-blush-white font-bold text-center uppercase">Nome do Usuário</h3>
+                    <img src={postagem.usuario?.foto} className="h-12 rounded-full" alt="" />
+                    <h3 className="text-lg  text-lavender-blush-white font-bold text-center uppercase">{postagem.usuario?.nome}</h3>
                 </div>
                 <div className="p-4">
-                    <h4 className="text-lg font-semibold uppercase">Título</h4>
-                    <p>texto</p>
-                    <p>Tema: </p>
-                    <p>Data: </p>
+                    <h4 className="text-lg font-semibold uppercase">{postagem.titulo}</h4>
+                    <p>{postagem.texto}</p>
+                    <p>Tema: {postagem.tema?.descricao}</p>
+                    <p>Data: {new Intl.DateTimeFormat(undefined, {
+                        dateStyle: 'full',
+                        timeStyle: 'medium',
+                    }).format(new Date(postagem.data))}</p>
                 </div>
             </div>
             <div className="flex">
