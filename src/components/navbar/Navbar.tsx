0a49db9c8fab4edf9a1e838e692bfe8cfce9fb57
CaciaRodrigues <caciarodriguues@gmail.com>
2025-01-22 15:59:01 -0300

    Alteração da Lógica de exibição Navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { ReactNode, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../contexts/AuthContext";
 
@@ -6,7 +6,7 @@ function Navbar() {
 
   const navigate = useNavigate();
 
-  const { handleLogout } = useContext(AuthContext)
+  const { usuario, handleLogout } = useContext(AuthContext)
 
   function logout() {
 
@@ -15,8 +15,10 @@ function Navbar() {
     navigate('/')
   }
 
-  return (
-    <>
+  let component: ReactNode;
+
+  if (usuario.token !== "") {
+    component = (
       <div className="w-full flex justify-around py-4 bg-dark-purple-brown text-mimi-pink" >
         <div className="container flex justify-between text-lg">
           <div className="flex gap-4">
@@ -35,6 +37,12 @@ function Navbar() {
           </div>
         </div>
       </div>
+    )
+  }
+
+  return (
+    <>
+      {component}
     </>
   );
 }
